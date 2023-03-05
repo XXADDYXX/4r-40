08a6db0343ae41673821a65ec1c80e7bcbeba4ca
Aadarsh Mane <onlyaddy68@gmail.com>
2023-03-06 01:01:39 +0530

    40 min

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:amazon/features/auth/auth_screen.dart';
+import 'package:amazon/router.dart';
+import 'package:flutter/material.dart';
+import 'package:amazon/constants/global_variables.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme:
+            const ColorScheme.light(primary: GlobalVariables.secondaryColor),
+        appBarTheme: const AppBarTheme(
+            elevation: 0, iconTheme: IconThemeData(color: Colors.black)),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
